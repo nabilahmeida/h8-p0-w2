d9531b3ce718d08386ce04f4242fb2388f583bbc
nabilahmeida <nabilah.m.aditya@gmail.com>
2018-04-24 18:15:04 +0700

    update exercises-4

--- exercises-3.js
+++ exercises-3.js
@@ -1,31 +1,30 @@
-var nama = 'nabilah';
-var peran = '';
+var nama = 'dimitri';
+var peran = 'raja';
 
-    if(nama === 'nabilah' && peran === ''){
+    if (nama === '' && peran === ''){
+        console.log('nama harus diisi!');
+    }
+    else if(nama === nama && peran === ''){
         console.log('Hallo '+nama, 'pilih peranmu untuk memulai game!');
     }
-    else if((nama == 'nabilah' && peran == 'ksatria')){
-        console.log('Selamat datang di Dunia Proxytia, '+nama);
-        console.log('Hallo ' +peran ,'' +nama, 'kamu dapat menyerang dengan senjatamu!');
-    }
-    else if (nama == 'nabilah' && peran == 'tabib'){
-        console.log('Selamat datang di Dunia Proxytia, '+nama);
-        console.log('Hallo ' +peran ,'' +nama, 'kamu akan membantu temanmu yang terluka.');
-    }
-    else if (nama == 'nabilah' && peran == 'penyihir'){
-        console.log('Selamat datang di Dunia Proxytia, '+nama);
-        console.log('Hallo '+peran , '' +nama, 'ciptakan keajaiban yang membantu kemenanganmu!')
-    }
-    else{
-        if (nama === '' && peran === '') {
-            console.log('Nama harus disi!');
+    else {
+        if((nama == nama && peran == 'ksatria')){
+            console.log('Selamat datang di Dunia Proxytia, '+nama);
+            console.log('Hallo ' +peran ,'' +nama, 'kamu dapat menyerang dengan senjatamu!');
         }
-        else if(nama === 'nabilah' && peran !=='ksatria'){
-            console.log('dan pilih salah satu nilai variable peran dengan 3 pilihan yaitu:');
-            console.log('ksatria, tabib, penyihir');
-            
+        else if (nama == nama && peran == 'tabib'){
+            console.log('Selamat datang di Dunia Proxytia, '+nama);
+            console.log('Hallo ' +peran ,'' +nama, 'kamu akan membantu temanmu yang terluka.');
+        }
+        else if(nama == nama && peran == 'penyihir'){
+            console.log('Selamat datang di Dunia Proxytia, '+nama);
+            console.log('Hallo '+peran , '' +nama, 'ciptakan keajaiban yang membantu kemenanganmu!');
+        }
+        else if(nama == nama && peran == peran){
+            console.log('Selamat datang di Dunia Proxytia, '+nama);
+            console.log('Hallo '+peran , '' +nama);
         }
         else {
-            console.log('maaf ya, tolong isi nama nabilah, karena nilai variable input manual');
+            console.log('Hallo ' +nama , 'pilih peranmu untuk memulai game!');
         }
     }
